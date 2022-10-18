f8096c91cd95e85cf313be8bbfeb075e9f24251f
mmorenoregalado <mmorenoregalado@gmail.com>
2022-10-18 09:20:54 -0600

    fix(Vue-directive): para solucionar error __dirname undefined en vite.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier',
-    '@vue/eslint-config-typescript/recommended'
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+    "@vue/eslint-config-typescript/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
